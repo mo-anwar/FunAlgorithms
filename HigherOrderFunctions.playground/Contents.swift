41657a2b364ec585a1bbe79c631a2e911791819c
Mohamed Anwar <manwar@mobilenoc.mobi>
2020-03-04 01:46:19 +0200

    Add CompactMap

--- HigherOrderFunctions.playground/Contents.swift
+++ HigherOrderFunctions.playground/Contents.swift
@@ -7,3 +7,8 @@ let filteredNumbers = numbers.filter { $0 == 4}
 let transformedNumbers = numbers.map { $0 * 3 }
 
 let sum = numbers.reduce(0, {$0 + $1})
+
+let optinalNumbers = [1, 2, 3, 4, nil, 4, 6, 7, 4,]
+
+let transformedOptinalNumbers = optinalNumbers.compactMap { $0 }
+
